c23eedaae43f17c9a3b5e53b449a0415ee14f972
Daniely Losi <112200546+Daniely-Losi@users.noreply.github.com>
2025-08-18 22:44:23 -0300

    Update events.ts
    Ajustando índices do objeto de eventos.

--- src/data/events.ts
+++ src/data/events.ts
@@ -4,49 +4,49 @@ import { Event } from '../types';
 export const events: Event[] = [
 
   {
-   id: 7,
+   id: 1,
     date: '2025-08-22',
     time: '08:00',
     format: 'Presencial' 
   },
   {
-   id: 8,
+   id: 2,
     date: '2025-09-09',
     time: '19:30',
     format: 'Virtual' 
   },
   {
-   id: 9,
+   id: 3,
     date: '2025-09-26',
     time: '08:00',
     format: 'Presencial' 
   },
   {
-   id: 10,
+   id: 4,
     date: '2025-10-14',
     time: '19:30',
     format: 'Virtual' 
   },
   {
-   id: 11,
+   id: 5,
     date: '2025-10-31',
     time: '08:00',
     format: 'Presencial' 
   },
   {
-   id: 12,
+   id: 6,
     date: '2025-11-11',
     time: '19:30',
     format: 'Virtual' 
   },
   {
-   id: 13,
+   id: 7,
     date: '2025-11-28',
     time: '08:00',
     format: 'Presencial' 
   },
   {
-   id: 14,
+   id: 8,
     date: '2025-12-12',
     time: '08:00',
     format: 'Presencial' 
