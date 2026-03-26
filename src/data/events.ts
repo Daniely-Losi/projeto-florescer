155ccf7b0daf595c685905570df2364654210c05
Daniely Losi <112200546+Daniely-Losi@users.noreply.github.com>
2026-03-26 20:29:18 -0300

    Update events.ts

--- src/data/events.ts
+++ src/data/events.ts
@@ -1,83 +1,96 @@
 import { Event } from '../types';
 
-// Mock data extraído da agenda 2026 do Projeto Florescer
+// Mock data extraído da agenda 2026 do Projeto Florescer com links e locais
 export const events: Event[] = [
   {
     id: 1,
     date: '2026-04-07',
     time: '19:30',
-    format: 'Virtual'
+    format: 'Virtual',
+    link: 'https://meet.google.com/ybd-jdxz-jjn'
   },
   {
     id: 2,
     date: '2026-04-24',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 3,
     date: '2026-05-29',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 4,
     date: '2026-06-02',
     time: '19:30',
-    format: 'Virtual'
+    format: 'Virtual',
+    link: 'https://meet.google.com/gmn-zxwu-pze'
   },
   {
     id: 5,
     date: '2026-06-26',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 6,
     date: '2026-07-31',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 7,
     date: '2026-08-04',
     time: '19:30',
-    format: 'Virtual'
+    format: 'Virtual',
+    link: 'https://meet.google.com/kav-krkt-pvt'
   },
   {
     id: 8,
     date: '2026-08-28',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 9,
     date: '2026-09-25',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 10,
     date: '2026-10-06',
     time: '19:30',
-    format: 'Virtual'
+    format: 'Virtual',
+    link: 'https://meet.google.com/grz-effy-svs'
   },
   {
     id: 11,
     date: '2026-10-30',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 12,
     date: '2026-11-27',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   },
   {
     id: 13,
     date: '2026-12-11',
     time: '08:00',
-    format: 'Presencial'
+    format: 'Presencial',
+    location: 'CEJUSC Blumenau'
   }
 ];
